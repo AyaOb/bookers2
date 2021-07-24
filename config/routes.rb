1e7158be5534ebda290a599e357ad3166b54c9c5
AyaOb <Skilpro2162@gmail.com>
2021-07-24 10:00:33 +0000

    [Add] users controller & route

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   devise_for :users
   resources :books, only: [:create, :index, :show, :edit, :update, :destroy]
-
+  resources :users, only: [:index, :show, :edit, :update]
 end
