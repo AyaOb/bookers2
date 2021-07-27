55b3e8af1f80252a2568e169b3e9bea58eb12510
AyaOb <Skilpro2162@gmail.com>
2021-07-27 07:09:16 +0000

    [Add] users/edit view

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'b6c1de0faca109a4b66e61f3410a091be77a928a7538c459ebc6974d9da86e8607ece22f4f5acfd287dfd462e709a028d2d23310247366cf16e7a4a8f04084d4'
